74069e7406acaeeda100053750983e5817e70bec
Tim Fuller <tim.fuller65@gmail.com>
2026-01-17 13:18:43 -0500

    Second shooter motor established

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.controls.VelocityVoltage;
@@ -23,15 +24,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.GeneralConstants;
 
 public class Shooter extends SubsystemBase {
+  
   // Declare constants
   private final double DRIVE_DEADBAND = 0.001; // Deadband for the drive motor. Values smaller than this will be rounded
                                                // to zero
 
   // Declare CAN ID for motor
-  private final int shooterMotorID = 21;
+  private final int shooterLeadID = 21;
+  private final int shooterFollowID = 22;
 
   // Declare motor variables
-  private TalonFX shooterMotor;
+  private TalonFX shooterMotorLead;
+  private TalonFX shooterMotorFollow;
 
   // Declare Phoenix PID controller gains
   private double drive_kG = 0.0;
@@ -43,29 +47,43 @@ public class Shooter extends SubsystemBase {
   private double drive_kD = 0.0;
 
   // Declare motor output requests
-  private final PositionVoltage m_positionRequest = new PositionVoltage(0).withSlot(0);
+  private final DutyCycleOut m_dutyRequest = new DutyCycleOut(0);
 
   /** Creates a new Shooter. */
   public Shooter() {
 
     // Create motors
-    shooterMotor = new TalonFX(shooterMotorID, GeneralConstants.CANBUS_NAME);
+    shooterMotorLead = new TalonFX(shooterLeadID, GeneralConstants.CANBUS_NAME);
+    shooterMotorFollow = new TalonFX(shooterFollowID, GeneralConstants.CANBUS_NAME);
 
-    // Create shooter motor configuration
-    var shooterConfigs = new TalonFXConfiguration();
+    // Configure motors
+    InitializeMotors();
 
-    // Set shooter motor output configuration
-    var shooterOutputConfigs = shooterConfigs.MotorOutput;
-    shooterOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
-    shooterOutputConfigs.NeutralMode = NeutralModeValue.Brake;
-    shooterOutputConfigs.withDutyCycleNeutralDeadband(DRIVE_DEADBAND);
+    // Set follower to follow lead motor
+    //shooterMotorFollow.setControl(new Follower(shooterMotorLead.getDeviceID(), true));
+  
+  }
+
+  /**
+   * Configure the motors
+   */
+  private void InitializeMotors() {
+
+    // Create lead shooter motor configuration
+    var shooterLeadConfigs = new TalonFXConfiguration();
+
+    // Set lead shooter motor output configuration
+    var shooterLeadOutputConfigs = shooterLeadConfigs.MotorOutput;
+    shooterLeadOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
+    shooterLeadOutputConfigs.NeutralMode = NeutralModeValue.Brake;
+    shooterLeadOutputConfigs.withDutyCycleNeutralDeadband(DRIVE_DEADBAND);
 
     // Set shooter motor feedback sensor
-    var shooterSensorConfig = shooterConfigs.Feedback;
+    var shooterSensorConfig = shooterLeadConfigs.Feedback;
     shooterSensorConfig.withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor);
 
     // Set shooter motor PID constants
-    var slot0Configs = shooterConfigs.Slot0;
+    var slot0Configs = shooterLeadConfigs.Slot0;
     slot0Configs.kG = drive_kG;
     slot0Configs.kS = drive_kS;
     slot0Configs.kV = drive_kV;
@@ -74,17 +92,43 @@ public class Shooter extends SubsystemBase {
     slot0Configs.kI = drive_kI;
     slot0Configs.kD = drive_kD;
 
-    // Apply shooter motor configuration and initialize position to 0
-    StatusCode shooterStatus = shooterMotor.getConfigurator().apply(shooterConfigs, 0.050);
-    if (!shooterStatus.isOK()) {
-      System.err.println("Could not apply shooter motor configs. Error code: " + shooterStatus.toString());
+    // Apply lead shooter motor configuration and initialize position to 0
+    StatusCode shooterLeadStatus = shooterMotorLead.getConfigurator().apply(shooterLeadConfigs, 0.050);
+    if (!shooterLeadStatus.isOK()) {
+      System.err.println("Could not apply shooter motor configs. Error code: " + shooterLeadStatus.toString());
       DriverStation.reportError("Could not apply shooter motor configs.", false);
     } else {
-      System.out.println("Successfully applied drive motor configs. Error code: " + shooterStatus.toString());
+      System.out.println("Successfully applied drive motor configs. Error code: " + shooterLeadStatus.toString());
     }
-    shooterMotor.getConfigurator().setPosition(0);
+    shooterMotorLead.getConfigurator().setPosition(0);
+
+
+
+    // Set follower shooter motor configuration
+    var shooterFollowConfigs = new TalonFXConfiguration();
+
+    //Set follower shooter output configuration
+    var shooterFollowOutputConfigs = shooterFollowConfigs.MotorOutput;
+    shooterFollowOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
+    shooterFollowOutputConfigs.NeutralMode = NeutralModeValue.Brake;
+    shooterFollowOutputConfigs.withDutyCycleNeutralDeadband(DRIVE_DEADBAND);
+
+
+    // Apply follower shooter motor configuration and initialize position to 0
+    StatusCode shooterFollowStatus = shooterMotorFollow.getConfigurator().apply(shooterFollowConfigs, 0.050);
+    if (!shooterFollowStatus.isOK()) {
+      System.out.println("Could not apply follower elevator motor configs. Error code: " + shooterFollowStatus.toString());
+      DriverStation.reportError("Could not apply follower elevator motor configs.", false);
+    } else {
+      System.out.println("Successfully applied follower elevator motor configs. Error code: " + shooterLeadStatus.toString());
+    }
+    shooterMotorFollow.getConfigurator().setPosition(0);
 
   }
+    
+  
+
+  
 
 
   @Override
