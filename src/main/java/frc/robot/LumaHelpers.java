f94db01281732fc120b75f15c0d6d91b327eb3f8
ChickenKing-III <linkgeig@gmail.com>
2026-02-14 13:44:22 -0500

    Started making Luma and Shooter range finders

---
+++ src/main/java/frc/robot/LumaHelpers.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class LumaHelpers {
+
+    /**
+     * Create new LumaHelpers class
+     */
+    public LumaHelpers(){
+
+    }
+
+    /**
+     * Get's target yaw angle
+     * @return
+     */
+    public double getYaw(){
+     return 0;   
+    }
+
+    /**
+     * Get's target distance
+     * @return
+     */
+    public double getDistance(){
+     return 0;   
+    }
+
+}
