2ea8bfe96a634e1259b7aa08fbcfae8fda9da667
Tim Fuller <tim.fuller65@gmail.com>
2026-02-18 07:19:35 -0500

    Documenting

--- src/main/java/frc/robot/commands/ResetRobot.java
+++ src/main/java/frc/robot/commands/ResetRobot.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// // NOTE: This command must be linked to driver station button ID LaunchpadSwitch1Bottom and LaunchpadSwitch1Top
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
